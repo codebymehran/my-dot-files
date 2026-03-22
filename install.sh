2ae519fc469a3305379418bcf25cc261e66e3624
Mehran Khan <mehran@mehrankhan.net>
2026-03-23 00:28:58 +1100

    Update install.sh
    added karabiner and snippets too

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+mkdir -p ~/.config
+mkdir -p ~/.config/karabiner
+mkdir -p ~/Library/Application\ Support/Code/User/snippets
+
 cp zshrc.sh ~/.zshrc
 cp wezterm.lua ~/.wezterm.lua
-mkdir -p ~/.config
 cp starship.toml ~/.config/starship.toml
+cp karabiner.json ~/.config/karabiner/karabiner.json
 cp settings.json ~/Library/Application\ Support/Code/User/settings.json
 cp keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+cp React_Snippets.code-snippets ~/Library/Application\ Support/Code/User/snippets/React_Snippets.code-snippets
+
 source ~/.zshrc
 echo "✅ Done"
